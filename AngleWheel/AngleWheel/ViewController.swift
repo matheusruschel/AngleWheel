dc2fa286ebf34aff82f135673ea8b76c41c0ec48
Matheus Ruschel <matheus.ruschel@gmail.com>
2019-01-16 20:14:04 -0500

    Setting up project

---
+++ AngleWheel/AngleWheel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AngleWheel
+//
+//  Created by Matheus Ruschel on 2019-01-16.
+//  Copyright © 2019 Matheus Ruschel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
